8299227440f0a22b11de9c97899a57f191b1adc0
yoshi-dharman <yoshi.dharman@ti.ukdw.ac.id>
2021-04-05 14:15:12 +0700

    hide imgbb key

--- src/redux/actions/submit.actions.js
+++ src/redux/actions/submit.actions.js
@@ -28,42 +28,66 @@ export const uploadAction = (image, event, setShow, setImage, setProgressBar) =>
     event.preventDefault();
     dispatch(uploadRequest());
 
-    const fd = new FormData();
-    fd.append('key', "35969ba19d2f852cb766c675ee5cdc58");
-    fd.append('image', image.file);
-
     return axios
-        .post("https://api.imgbb.com/1/upload", fd, {
-            onUploadProgress: ProgressEvent => {
-                // console.log('Upload progress: ' + Math.round(ProgressEvent.loaded / ProgressEvent.total * 100) + '%');
-                setProgressBar(Math.round(ProgressEvent.loaded / ProgressEvent.total * 100));
+        .get("https://art-share-app.herokuapp.com/key/imgbb", {
+            headers: {
+                "Authorization" : `Bearer ${localStorage.token}`
             }
         })
         .then(result => {
-            const date = result.data.data.time + "000";
-            const d = new Date(parseInt(date))
+            const fd = new FormData();
+            fd.append('key', result.imgbb_key);
+            fd.append('image', image.file);
 
-            const dataImage = {
-                user_id : JSON.parse(localStorage.payload)._id,
-                image_url : result.data.data.url,
-                title: image.title,
-                time: d.toUTCString()
-            }
-
-            setImage({
-                title: "",
-                file: null,
-                status: "",
-                disable: "",
+            axios
+            .post("https://api.imgbb.com/1/upload", fd, {
+                onUploadProgress: ProgressEvent => {
+                    // console.log('Upload progress: ' + Math.round(ProgressEvent.loaded / ProgressEvent.total * 100) + '%');
+                    setProgressBar(Math.round(ProgressEvent.loaded / ProgressEvent.total * 100));
+                }
             })
-            setProgressBar(0);
-
-            axios.post("https://art-share-app.herokuapp.com/image/", dataImage)
             .then(result => {
-                setShow({
-                    valid: true,
-                    invalid: false
-                });
+                const date = result.data.data.time + "000";
+                const d = new Date(parseInt(date))
+
+                const dataImage = {
+                    user_id : JSON.parse(localStorage.payload)._id,
+                    image_url : result.data.data.url,
+                    title: image.title,
+                    time: d.toUTCString()
+                }
+
+                setImage({
+                    title: "",
+                    file: null,
+                    status: "",
+                    disable: "",
+                })
+                setProgressBar(0);
+
+                axios.post("https://art-share-app.herokuapp.com/image/", dataImage)
+                .then(result => {
+                    setShow({
+                        valid: true,
+                        invalid: false
+                    });
+                })
+                .catch(err => {
+                    setShow({
+                        valid: false,
+                        invalid: true
+                    });
+                    setImage({
+                        title: "",
+                        file: null,
+                        status: "",
+                        disable: "",
+                    })
+                    console.log(err);
+                    dispatch(uploadFailed(err))
+                })
+                
+                dispatch(uploadSuccess());
             })
             .catch(err => {
                 setShow({
@@ -76,25 +100,14 @@ export const uploadAction = (image, event, setShow, setImage, setProgressBar) =>
                     status: "",
                     disable: "",
                 })
+                setProgressBar(0);
                 console.log(err);
                 dispatch(uploadFailed(err))
             })
-            
-            dispatch(uploadSuccess());
+
         })
-        .catch(err => {
-            setShow({
-                valid: false,
-                invalid: true
-            });
-            setImage({
-                title: "",
-                file: null,
-                status: "",
-                disable: "",
-            })
-            setProgressBar(0);
-            console.log(err);
+        .catch(e => {
+            console.log(e);
             dispatch(uploadFailed(err))
         })
 };
